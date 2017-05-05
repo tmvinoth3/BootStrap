caff6aced7ef51bf4feab7c0c7a0449c599c2175
TM <575435@cognizant.com>
2017-05-05 15:11:26 +0530

    First Commit

---
+++ FirstBoot/js/site.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+    var $pick = $("#pick");
+    $("#dropdown li a").on("click", function () {
+        $pick.text(this.text);
+    });
+    $("#submit").tooltip();
+    $(".carousel").carousel('cycle');
+})();
